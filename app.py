e220e669be808427a786edc4e6e9eb415a83a174
jgage930 <jgage930@gmail.com>
2022-01-20 10:06:31 -0600

    spell-view working

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
 import requests
+import json
+from models.spell import Spell
 
 app = Flask(__name__)
 global base_url
@@ -21,6 +23,50 @@ def index():
         spells[str(i)] = getSpells(i)
     
     return render_template('main.html', spells=spells)
+
+@app.route('/<spell_name>')
+def spell_view(spell_name):
+    # find the spell name and get data from the json file
+    spell_data = {}
+    with open('data.json', 'r') as f:
+        spell_data = json.load(f)
+
+    if spell_name in spell_data:
+        # current spell
+        my_data = spell_data[spell_name]
+        
+        # get data to put into Spell object
+        name = spell_name
+        desc = ' '.join([str(elem) for elem in my_data['desc']])
+        range = my_data['range']
+        components = ' '.join([str(elem) for elem in my_data['components']])
+        
+        if 'material' in my_data:
+            material = my_data['material']
+        else:
+            material = ''
+
+        if my_data['ritual'] == 'false':
+            ritual = 'No'
+        else:
+            ritual = 'Yes'
+
+        duration = my_data['duration']
+        
+        if my_data['concentration'] == 'false':
+            concentration = 'No'
+        else:
+            concentration = 'Yes'
+
+        casting_time = my_data['casting_time']
+        level = my_data['level']
+        school = my_data['school']['name']
+
+        spell = Spell(name, desc, range, components, material, ritual, duration, concentration, casting_time, level, school)
+        return render_template('spell_view.html', spell=spell)
+    return render_template('error.html')            
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ models/spell.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+
+@dataclass
+class Spell:
+
+    # spell atributes
+    name: str
+    desc: str
+    range: str
+    components: str
+    material: str
+    ritual: str
+    duration: str
+    concentration: str
+    casting_time: str
+    level: str
+    school: str
